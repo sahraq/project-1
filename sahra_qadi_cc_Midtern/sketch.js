5b27a66a492694ec9ae008c2ac5ebbc6ee70d039
sahraq <soq204@nyu.edu>
2024-10-26 11:36:11 -0400

    add character movements

--- sahra_qadi_cc_Midtern/sketch.js
+++ sahra_qadi_cc_Midtern/sketch.js
@@ -1,17 +1,34 @@
-//confusing background
-function drawBackground() {
-  noStroke();
-  for (let i = 0; i < 20; i++) {
-    fill(random(255), random(255), random(255), 50);
-    let x = random(width);
-    let y = random(height);
-    let w = random(100, 400); 
-    let h = random(100, 400);
-    ellipse(x, y, w, h);
+let shapeCount = 10; // number of shapes
+let shapeSize = 50; // base size of shapes
+let shapeArray = []; // hold shape instances
+
+class ConfusionShape {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.angle = random(TWO_PI);
+    this.speed = random(1, 3);
+    this.wavyOffset = random(TWO_PI);
   }
-}
-function display() {
-    // characters
+  
+  update() {
+    this.x += cos(this.angle) * this.speed;
+    this.y += sin(this.angle) * this.speed;
+
+    // wrap around the edges
+    if (this.x > width) this.x = 0;
+    if (this.x < 0) this.x = width;
+    if (this.y > height) this.y = 0;
+    if (this.y < 0) this.y = height;
+
+    // change angle randomly every 60 frames
+    if (frameCount % 60 === 0) {
+      this.angle += random(-PI / 4, PI / 4);
+    }
+  }
+  
+  display() {
     fill(random(255), random(255), random(255), 150);
     stroke(0);
     strokeWeight(2);
@@ -22,4 +39,35 @@ function display() {
       vertex(this.x + xOffset, this.y + yOffset);
     }
     endShape(CLOSE);
+  }
+}
+
+function setup() {
+  createCanvas(800, 600);
+  for (let i = 0; i < shapeCount; i++) {
+    shapeArray.push(new ConfusionShape(random(width), random(height), shapeSize));
+  }
+}
+
+function draw() {
+  // background with shapes for a confusing effect
+  drawBackground();
+  
+  for (let shape of shapeArray) {
+    shape.update();
+    shape.display();
+  }
+}
+
+// confusing background pattern
+function drawBackground() {
+  noStroke();
+  for (let i = 0; i < 20; i++) {
+    fill(random(255), random(255), random(255), 50);
+    let x = random(width);
+    let y = random(height);
+    let w = random(100, 400); 
+    let h = random(100, 400);
+    ellipse(x, y, w, h);
+  }
 }
